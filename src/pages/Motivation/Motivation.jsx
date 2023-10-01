680275a218aa364001875c6aede844e584157db0
Ndavhe Tshivhase <ndavhe89@gmail.com>
2023-10-01 20:59:35 +0200

    add notes taking

--- src/pages/Motivation/Motivation.jsx
+++ src/pages/Motivation/Motivation.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { AiOutlineBook } from 'react-icons/ai';
-
+import About3 from '../../assets/About-3.png'
 
 const Motivation = () => {
 	const [quote, setQuote] = useState({});
@@ -9,7 +9,7 @@ const Motivation = () => {
 	useEffect(() => {
 		axios.get("https://api.quotable.io/random")
 			.then((response) => {
-				setQuote(response.data); 
+				setQuote(response.data);
 			})
 			.catch((error) => {
 				console.log(error);
@@ -17,22 +17,19 @@ const Motivation = () => {
 	}, []);
 
 	return (
-		<div className='grid h-screen place-items-center overflow-hidden z-[1] p-6 w-screen h-full relative'>
-			<div className='font-Karla flex flex-col justify-center items-center gap-6'>
-				<p className='text-focus-in text-3xl lg:text-[64px] font-bold text-center'>	Quote of the day</p> 
+		<div className="slide-in-fwd-center flex flex-col justify-center items-center w-screen relative top-0 font-bold contain">
+			<div className='font-Karla flex flex-col justify-center items-center gap-1'>
+				<p className='text-focus-in text-3xl lg:text-[64px] font-bold text-center'>	Quote of the day</p>
 			</div>
-			<div className="flex flex-col justify-center max-w-xs p-8 shadow-xl rounded-xl sm:px-12 dark:bg-white dark:text-black">
-				 <AiOutlineBook className="w-8 h-8" />
-				<hr className='border-black border-1' />
-				<br />
-				<div key={quote?.id}>
+			<div className='flex flex-row w-full justify-center items-center gap-5 p-4'>
+				<div className=' border-[1px] border-black p-2x relative gap-2' >
+					<img src={About3} alt="About" width={300} height={250} />
+					<p className='font-Karla font-medium text-xl w-1/2' style={{ mr: 1 }}>"{quote?.content}"</p>
 					<br />
-					<h1>"{quote?.content}"</h1>
-					<br />
-					<h3><i>{quote?.author}</i></h3>
+					<i>{quote?.author}</i>
 				</div>
 			</div>
-		</div>
+		</div >
 	);
 };
 
--- src/pages/Notes/NotesTaker.jsx
+++ src/pages/Notes/NotesTaker.jsx
@@ -1,27 +1,63 @@
-import React, { useState, useEffect } from "react"; 
+import React, { useState, useEffect } from "react";
 import NotesTakerEditor from './NotesTakerEditor';
+import { useData } from './Context'
+import Card from "./Card";
 
 const NotesTaker = () => {
-    const [secondsRemaining, setSecondsRemaining] = useState(600);
-    const [notes, setNotes] = useState("");
+    const [clicked, setClicked] = useState(false);
+    const [data, setData] = useState([]);
+    const [dataCard, setDataCard] = useState();
+    const [currentNoteId, setCurrentNoteId] = useState(data[0] ? data[0].id : "");
+
+
 
     useEffect(() => {
-        if (secondsRemaining > 0) {
-            const interval = setInterval(() => {
-                setSecondsRemaining(secondsRemaining - 1);
-            }, 1000);
+        const localNotes = localStorage.getItem("notes");
+        if (localNotes !== null) {
+            setData(JSON.parse(localNotes));
 
-            return () => clearInterval(interval);
+            setDataCard(data.map((note, idx) =>
+                <Card key={note.id}
+                    id={note.id}
+                    title={note.title}
+                    color={note.currentColor}
+                    body={note.body}
+                    date={note.date}
+                    currentNoteId={setCurrentNoteId}
+                    change={setClicked} />));
         }
-    }, [secondsRemaining]);
-
-    const handleNotesChange = (e) => {
-        setNotes(e.target.value);
-    };
+        //setData(JSON.parse(localStorage.getItem('notes')));
+    }, [data])
 
     return (
-        <div className="flex flex-col justify-center items-center">
-            <NotesTakerEditor notes={notes} onNotesChange={handleNotesChange} />
+
+        <div className="z-[1] p-6 w-screen h-full bg-[#CFB5E2] relative" >
+            <div style={{ filter: clicked ? 'blur(8px)' : 'none' }}>
+                <p className="font-Inter font-bold text-4xl sm:text-5xl md:text-6xl lg:text-[64px]">
+                    Hi, what's on your mind?
+                </p>
+                <div className="flex flex-col sm:flex-row justify-between w-full items-center gap-3">
+                    <div className="flex flex-row justify-center items-center gap-2">
+                        <button
+                            className="rounded-full w-[130px] h-[30px] border-[3px] border-black text-center text-white bg-black"
+                            onClick={() => setClicked(true)}
+                        >
+                            Create
+                        </button>
+                        <button className="rounded-full w-[130px] h-[30px] border-[3px]
+                         border-black text-center text-white bg-black"  onClick={() => {
+                                localStorage.clear();
+                                setData([]);
+                            }}>
+                            Clear
+                        </button>
+                    </div>
+                </div>
+                <div className="grid grid-cols-2 md:grid-cols-4 gap-3 mt-2 w-full min-h-[200px]">
+                    {data[0] ? dataCard : <p>Create a new Note</p>}
+                </div>
+            </div>
+            <NotesTakerEditor Clicked={clicked} change={setClicked} currentNoteId={currentNoteId} />
         </div>
     );
 };
